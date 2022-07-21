d2e44ddfc30982b99f246f873b84c433eb7329c3
tiago369 <tiagobarreto581@gmail.com>
2022-07-21 16:57:38 -0300

    fix dobotserver 6 function

--- dobot_ws/src/dobot/src/DobotServer.cpp
+++ dobot_ws/src/dobot/src/DobotServer.cpp
@@ -114,9 +114,13 @@ void InitDeviceInfoServices(ros::NodeHandle &n, std::vector<ros::ServiceServer> 
 /*
  * Pose
  */
-#include "dobot/GetPose.h"
 
-bool GetPoseService(dobot::GetPose::Request &req, dobot::GetPose::Response &res)
+// 6 function
+// #include "dobot/GetPose.h"
+#include "dobot/srv/get_pose.hpp"
+
+void GetPoseService(const std::shared_ptr<dobot::GetPose::Request> req, 
+                          std::shared_ptr<dobot::GetPose::Response> res)
 {
     Pose pose;
 
@@ -127,11 +131,11 @@ bool GetPoseService(dobot::GetPose::Request &req, dobot::GetPose::Response &res)
         res.z = pose.z;
         res.r = pose.r;
         for (int i = 0; i < 4; i++) {
-            res.jointAngle.push_back(pose.jointAngle[i]);
+            res.joint_angle.push_back(pose.joint_angle[i]);
         }
     }
 
-    return true;
+    // return true;
 }
 
 void InitPoseServices(ros::NodeHandle &n, std::vector<ros::ServiceServer> &serverVec)
